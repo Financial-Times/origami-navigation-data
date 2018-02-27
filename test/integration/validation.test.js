38434ca8bfea9e0ab4071b8ea93a1ef196dee431
Jake Champion <me@jakechampion.name>
2018-02-27 17:31:19 +0000

    Update validation.test.js

--- test/integration/validation.test.js
+++ test/integration/validation.test.js
@@ -47,7 +47,17 @@ describe('Navigation data', () => {
                                                                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
                                                         }
                                                 }
-                                        ).then(statusCode => assert.equal(statusCode, 200));
+                                        )
+					.then(statusCode => assert.equal(statusCode, 200))
+					.catch(function() {
+						return heads(url,
+                                         	       {
+                                                	        headers: {
+                                                        	        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+	                                                        }
+        	                                        }
+                	                        ).then(statusCode => assert.equal(statusCode, 200));
+					});
 				});
 			});
 		});
